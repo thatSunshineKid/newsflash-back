5f0718802f2be14811ca46bd4f0554d83fe16a6d
Adam Sunshine <adam@csuac.com>
2018-12-12 14:58:07 -0700

    took whitenoise out of wsgi file and left in settings, per update notes

--- newsflash_back/wsgi.py
+++ newsflash_back/wsgi.py
@@ -16,5 +16,3 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "newsflash_back.settings")
 application = get_wsgi_application()
 
 
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
