cccdbbc1aa22940326ada18f2c8fcbdfb6661993
Adam Sunshine <adam@csuac.com>
2018-12-15 14:54:12 -0700

    added initial user, author, post, tag, subtag model classes with migration

--- backend/admin.py
+++ backend/admin.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import Author, Source, Post, Tag, Subtag
+
 # Register your models here.
+
+admin.site.register(Author)
+admin.site.register(Source)
+admin.site.register(Post)
+admin.site.register(Tag)
+admin.site.register(Subtag)
