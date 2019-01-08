7dff77a9aa13db64ed2af5166f3c99010996c4b7
Adam Sunshine <adam@csuac.com>
2019-01-08 14:42:29 -0700

    added and tested logout logoutall view for knox authentication token

--- backend/urls.py
+++ backend/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
 from backend import views
 from rest_framework import routers
+from knox.views import LogoutView, LogoutAllView
 
 
 router = routers.DefaultRouter()
@@ -10,6 +11,8 @@ urlpatterns = [
     url("^auth/register/$", views.RegistrationAPI.as_view()),
     url("^auth/login/$", views.LoginAPI.as_view()),
     url("^auth/user/$", views.UserAPI.as_view()),
+    url("auth/logout/$", LogoutView.as_view(), name='knox_logout'),
+    url("auth/logoutall/$", LogoutAllView.as_view(), name='knox_logoutall'),
     url('test/', views.NewsbotList.as_view()),
     url('tech/', views.TechpostList.as_view()),
     url('sports/', views.SportspostList.as_view()),
